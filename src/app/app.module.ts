4aa4adc43a6aade4066c6d745e0da03332d0262a
Neena Thomas <neena.thomas7@gmail.com>
2022-10-27 08:12:47 +0400

    removing component declarations from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { MediaComponent } from './components/media/media.component';
-import { SupportComponent } from './components/support/support.component';
-import { TestimonialComponent } from './components/testimonial/testimonial.component';
-import { OfferComponent } from './components/offer/offer.component';
-import { ContentComponent } from './components/content/content.component';
+
+// import { HeaderComponent } from './components/header/header.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { HomeComponent } from './components/home/home.component';
+// import { MediaComponent } from './components/media/media.component';
+// import { SupportComponent } from './components/support/support.component';
+// import { TestimonialComponent } from './components/testimonial/testimonial.component';
+// import { OfferComponent } from './components/offer/offer.component';
+// import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    MediaComponent,
-    SupportComponent,
-    TestimonialComponent,
-    OfferComponent,
-    ContentComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // HomeComponent,
+    // MediaComponent,
+    // SupportComponent,
+    // TestimonialComponent,
+    // OfferComponent,
+    // ContentComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
